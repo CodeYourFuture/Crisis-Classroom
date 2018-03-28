8eb464b03bc46485f7cfebdc606372db36fcf446
Mohsen <30389896+Moradi66@users.noreply.github.com>
2018-03-28 11:14:16 +0100

    push files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class CrisisClassrom extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Alpha Code team</h1>
+          <ul>
+            <li>Mona</li>
+            <li>Amir</li>
+            <li>Mohsen</li>
+          </ul>
+        </header>
+        <p className="App-intro">
+          Mona for start work on README.md file ,,thanks
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CrisisClassrom;

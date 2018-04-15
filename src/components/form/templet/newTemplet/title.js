d5d79344757c90397fb68cd735603fde49d21c2b
ameerkabir <ameerkabir2@gmail.com>
2018-04-15 13:57:35 +0100

    changes made in form

--- src/components/form/templet/newTemplet/title.js
+++ src/components/form/templet/newTemplet/title.js
@@ -118,7 +118,7 @@ export default class LessonTitle extends React.Component {
               type="text"
               name="numberOfPeople"
               onChange={this.onChange}
-              placeholder="How many people is it for?"
+              placeholder="How many people is it for?"pu
               value={this.state.info.numberOfPeople}
             />
             &nbsp;

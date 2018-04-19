997ddd3107b67abebdeda25dadd75eda6180120b
Moradi66 <mohsen000069@gmail.com>
2018-04-19 18:24:56 +0100

    ?????

--- src/components/form/templet/newTemplet/NextForm.js
+++ src/components/form/templet/newTemplet/NextForm.js
@@ -9,30 +9,29 @@ export default class NexForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fields: []    
+      fields: []
     };
   }
-  add = name  => {
+  add = name => {
     this.setState({
       fields: [...this.state.fields, name]
     });
   };
   render() {
-      console.log(this.state.fields)
+    console.log(this.state.fields);
     return (
-        <div>
-        { this.state.fields && 
-            this.state.fields.map(fields => {
-          return(  <div> <input type="text" /> 
-                   <input type="file"/> 
-          </div>
-                   
-        
-        )
-          
-           })
-     }
-    <button onClick={this.add} > add</button>
-    </div>);
+      <div>
+        {this.state.fields &&
+          this.state.fields.map(fields => {
+            return (
+              <div>
+                <input type="text" />
+                <input type="file" />
+              </div>
+            );
+          })}
+        <button onClick={this.add}> add</button>
+      </div>
+    );
   }
 }

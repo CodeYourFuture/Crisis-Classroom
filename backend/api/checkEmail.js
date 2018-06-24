54074fe4cee259d8ad25478626a473b435ee16f4
Mohsen <30389896+Moradi66@users.noreply.github.com>
2018-06-24 18:20:27 +0100

    Back-end errors (#37)
    * new branch
    
    * Hashing Passwords with Bcrypt done
    
    * fixed reset passsword
    
    * handell bakend eerror
    
    * add some status in back-end
    
    * fix comflects
    
    * crisisdb.sqlite file

--- backend/api/checkEmail.js
+++ backend/api/checkEmail.js
@@ -1,16 +1,19 @@
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require('sqlite3').verbose();
 
-const filename = "./database/crisisdb.sqlit";
+const filename = './database/crisisdb.sqlit';
 let db = new sqlite3.Database(filename);
 
 const checkEmail = (req, res) => {
-  var sql = "select users.email from users where  email=?";
+  var sql = 'select users.email from users where  email=?';
   db.all(sql, [req.body.email], (err, rows) => {
     if (err) {
-      res.status(500);
+      return res.status(400).json({
+        msg:
+          'Ops! Sorry something happened on the server, please try again later.',
+      });
     } else {
       res.status(200).json({
-        rows
+        rows,
       });
     }
   });
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
-require("dotenv").config()
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-const bodyparser = require("body-parser");
-const cors = require("cors");
-const apiRouter = require("./api");
+require('dotenv').config();
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const apiRouter = require('./api');
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-app.use("/", apiRouter());
+app.use('/', apiRouter());
 
 app.listen(process.env.PORT || 8080);

1c7559d18ef873e3e3ed61e24566b13fb6e02dac
Moradi66 <mohsen000069@gmail.com>
2018-07-10 10:16:45 -0700

    admin info

--- frontend/src/components/admin/admin.jsx
+++ frontend/src/components/admin/admin.jsx
@@ -1,52 +1,93 @@
 import React from 'react';
 import axios from 'axios';
 import decode from 'jwt-decode';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AuthService from '../../Auth/AuthService';
 
 class UserInfo extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
+      admin: 0,
+      email: '',
+      firstName: '',
+      surName: '',
+      teacher: 0,
+      userName: '',
       isAdmin: null,
       err: '',
       msg: '',
     };
   }
-  UNSAFE_componentWillMount () {
-    const token = AuthService.getToken ();
-    const decoded = decode (token);
+  UNSAFE_componentWillMount() {
+    const token = AuthService.getToken();
+    const decoded = decode(token);
     const userName = decoded.userName;
     axios
-      .post (`${process.env.REACT_APP_DOMAIN}/admin`, {userName})
-      .then (result => {
+      .post(`${process.env.REACT_APP_DOMAIN}/admin`, { userName })
+      .then((result) => {
         if (result) {
-          const isAdmin = result.data;
-          this.setState ({isAdmin});
+          const {
+            firstName,
+            surName,
+            userName,
+            email,
+            teacher,
+            admin,
+          } = result.data[0];
+          this.setState({
+            firstName,
+            surName,
+            userName,
+            email,
+            teacher,
+            admin,
+          });
         }
       })
-      .catch (err => {
+      .catch((err) => {
         if (err.msg) {
-          this.setState ({err: err.msg});
+          this.setState({ err: err.msg });
         } else {
-          this.setState ({
-            err: 'Ops! Sorry something happened on the server, please try again later',
+          this.setState({
+            err:
+              'Ops! Sorry something happened on the server, please try again later',
           });
         }
       });
   }
-  render () {
-    const {isAdmin, err, msg} = this.state;
+  render() {
+    const {
+      firstName,
+      surName,
+      userName,
+      email,
+      admin,
+      err,
+      msg,
+    } = this.state;
     return (
       <div>
         <div className="login-form">
-          {isAdmin
-            ? <div>
+          {admin ? (
+            <div>
+              <div>
+                <h5>First Name: {firstName}</h5>
+                <h5>Sur Name: {surName}</h5>
+                <h5>User Name: {userName}</h5>
+                <h5>Email: {email}</h5>
+              </div>
+              <div>
                 <Link to="/users-info" className="btn btn-outline-dark">
                   Users Info
                 </Link>
               </div>
-            : <h5> {err} {msg}</h5>}
+            </div>
+          ) : (
+            <h5>
+              {err} {msg}
+            </h5>
+          )}
         </div>
       </div>
     );

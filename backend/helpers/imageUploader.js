6cc2da82e42c6b80004980a1ac2d62a8f08f7155
Moradi66 <mohsen000069@gmail.com>
2018-07-29 18:44:56 +0100

    BUCKET

--- backend/helpers/imageUploader.js
+++ backend/helpers/imageUploader.js
@@ -13,7 +13,7 @@ var s3 = new aws.S3 (config);
 var upload = multer ({
   storage: multerS3 ({
     s3: s3,
-    bucket: 'crisis-class-room',
+    bucket: process.env.BUCKET,
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
@@ -26,3 +26,10 @@ var upload = multer ({
 });
 
 module.exports = upload;
+// PRE avatar/
+// PRE ingredients/
+// PRE instructions/
+// PRE lesson_title/
+// PRE number_of_people/
+// PRE time_to_prepare/
+// PRE tools/

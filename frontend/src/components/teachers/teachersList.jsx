3bcb0d26ead532ce494cd0f9f2d80461ff23f9c8
Moradi66 <mohsen000069@gmail.com>
2018-08-01 16:18:37 +0100

    crate filter for teacher page

--- frontend/src/components/teachers/teachersList.jsx
+++ frontend/src/components/teachers/teachersList.jsx
@@ -1,57 +1,153 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import Input from "../input";
-function searchingFor(term) {
-  return teachers => {
-    const result = teachers.skills
-      .map(skill => skill.skill_name.includes(term))
-      .filter(index => index === true);
-    return !term || result[0];
-  };
-}
 
 export default class TeacherslatesList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: null
+      skillName: null,
+      skillLevel: null,
+      WhatExperience: null,
+      Whatplace: null,
+      Collage: null
     };
   }
   searchHandler = e => {
     this.setState({
-      term: e.target.value,
+      [e.target.name]: e.target.value,
       result: e.target.value.length ? true : false
     });
   };
 
   render() {
+    const {
+      skillName,
+      skillLevel,
+      WhatExperience,
+      Whatplace,
+      Collage
+    } = this.state;
     const teachers = this.props.teachers;
-    const searchResult = teachers.filter(searchingFor(this.state.term));
+    //search For Skill Name
+    let skillNames = [];
+    const searchResultForSkillName = teachers.filter(
+      searchingForSkillName(skillName)
+    );
+    //search For Skill Level
+    const searchResultForSkillLevel = searchResultForSkillName.filter(
+      searchingForSkillLevel(skillLevel)
+    );
+    //search For Experiences
+    const searchResultForWhatExperience = searchResultForSkillLevel.filter(
+      searchingForWhatExperience(WhatExperience)
+    );
+    let whatExperiences = [];
+    //search For places they work in
+    const searchResultForWhatplace = searchResultForWhatExperience.filter(
+      searchingForWhatplace(Whatplace)
+    );
+    let Whatplaces = [];
+    //search For Collages
+    const searchResultForCollages = searchResultForWhatplace.filter(
+      searchingForCollages(Collage)
+    );
+    let Collages = [];
+    // with_whom_teacher;
+    teachers.map(teacher => {
+      return (
+        //search For Skill Name
+        teacher.skills.map(skill => {
+          return skillNames.push(skill.skill_name);
+        }),
+        //search For Experiences
+        teacher.experiences.map(experience => {
+          return whatExperiences.push(experience.what_experience);
+        }),
+        //search For places they work in
+        teacher.experiences.map(experience => {
+          return Whatplaces.push(experience.what_place);
+        }),
+        //search For Collages
+        teacher.experiences.map(experience => {
+          return Collages.push(experience.with_whom_teacher);
+        })
+      );
+    });
     return (
       <div>
         <h1>Teachers</h1>
         <div>
-          <select onChange={this.searchHandler} className="form-control">
-            <option>Skill</option>
-            {teachers.map(teacher => {
-              return teacher.skills.map((skill, i) => {
-                return (
-                  <option value={skill.skill_name} key={i}>
-                    {skill.skill_name}
-                  </option>
-                );
-              });
+          <select
+            onChange={this.searchHandler}
+            name="skillName"
+            className="form-control"
+          >
+            <option value="">Skills</option>
+            {Array.from(new Set(skillNames)).map((name, i) => {
+              return (
+                <option value={name} key={i}>
+                  {name}
+                </option>
+              );
             })}
           </select>
-          {/* <Input
+          <select
+            name="skillLevel"
             onChange={this.searchHandler}
-            type="text"
-            placeholder="Search ...."
-          /> */}
+            className="form-control"
+          >
+            <option value="">Skill level</option>
+            <option value="Beginner">Beginner</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Advance">Advance</option>
+          </select>
+          <select
+            onChange={this.searchHandler}
+            name="WhatExperience"
+            className="form-control"
+          >
+            <option value="">Experiences</option>
+            {Array.from(new Set(whatExperiences)).map((name, i) => {
+              return (
+                <option value={name} key={i}>
+                  {name}
+                </option>
+              );
+            })}
+          </select>
+          <select
+            onChange={this.searchHandler}
+            name="Whatplace"
+            className="form-control"
+          >
+            <option value="">Industry</option>
+            {Array.from(new Set(Whatplaces)).map((name, i) => {
+              return (
+                <option value={name} key={i}>
+                  {name}
+                </option>
+              );
+            })}
+          </select>
+          <select
+            onChange={this.searchHandler}
+            name="Collage"
+            className="form-control"
+          >
+            <option value="">Collages</option>
+            {Array.from(new Set(Collages)).map((name, i) => {
+              return (
+                <option value={name} key={i}>
+                  {name}
+                </option>
+              );
+            })}
+          </select>
         </div>
         &nbsp; &nbsp;
         <div>
-          {searchResult.map((e, i) => (
+          {searchResultForCollages.map((e, i) => (
             <div key={i}>
               <Link to={`/teachers/${e.id}`}>
                 {!e.avatar ? (
@@ -83,4 +179,51 @@ export default class TeacherslatesList extends Component {
       </div>
     );
   }
+}
+//search For Skill Name
+function searchingForSkillName(skillName) {
+  return teachers => {
+    const result = teachers.skills
+      .map(skill => skill.skill_name.includes(skillName))
+      .filter(index => index === true);
+    return !skillName || result[0];
+  };
+}
+
+//search For Skill Level
+function searchingForSkillLevel(skillLevel) {
+  return teachers => {
+    const result = teachers.skills
+      .map(skill => skill.skill_level.includes(skillLevel))
+      .filter(index => index === true);
+    return !skillLevel || result[0];
+  };
+}
+
+//search For Experiences
+function searchingForWhatExperience(WhatExperience) {
+  return teachers => {
+    const result = teachers.experiences
+      .map(experience => experience.what_experience.includes(WhatExperience))
+      .filter(index => index === true);
+    return !WhatExperience || result[0];
+  };
+}
+//search For places they work in
+function searchingForWhatplace(Whatplace) {
+  return teachers => {
+    const result = teachers.experiences
+      .map(experience => experience.what_place.includes(Whatplace))
+      .filter(index => index === true);
+    return !Whatplace || result[0];
+  };
+}
+//search For Collages
+function searchingForCollages(Collage) {
+  return teachers => {
+    const result = teachers.experiences
+      .map(experience => experience.with_whom_teacher.includes(Collage))
+      .filter(index => index === true);
+    return !Collage || result[0];
+  };
 }
